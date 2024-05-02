c80f9118cc42ee2c23c77005d072b82ac586c357
SunayNag <sunay.nagpure@gmail.com>
2024-05-03 02:41:56 +0530

    completion

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "project-exhibithion-2.firebaseapp.com",
+  projectId: "project-exhibithion-2",
+  storageBucket: "project-exhibithion-2.appspot.com",
+  messagingSenderId: "282393308344",
+  appId: "1:282393308344:web:00e0a819ad73d83b69584d"
 };
 
 // Initialize Firebase
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -101,7 +101,7 @@ export default function Listing() {
               {listing.address}
             </p>
             <div className='flex gap-4'>
-              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+              <p className='bg-red-900 w-full max-w-[200%] text-white text-center p-1 rounded-md'>
                 {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
               </p>
               {listing.offer && (
